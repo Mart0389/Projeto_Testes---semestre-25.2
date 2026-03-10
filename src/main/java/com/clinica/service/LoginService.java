9c7c3f7c9e89d57cd3a06e1060cc658f9ef6e64f
Marta <martinha03luna@gmail.com>
2026-03-10 07:22:01 -0300

    TC005 - teste green

--- src/main/java/com/clinica/service/LoginService.java
+++ src/main/java/com/clinica/service/LoginService.java
@@ -18,20 +18,30 @@ public class LoginService {
         return "O campo login é obrigatório";
     }
 
-    // Validação da Senha (TC_004 - Implementação para o GREEN)
+// Validação da Senha (TC_004 - Implementação para o GREEN)
         if (senha == null || senha.trim().isEmpty()) {
             return "O campo senha é obrigatório";
         }
 
         Usuario usuario = repository.buscarPorLogin(login);
         
-             
-       //if (usuario != null) //-- adicionado para testar o tc002, tcc003
-        
-       if (usuario != null && usuario.getSenha().equals(senha)) {
-
-            return "Bem vindo ao sistema";
-        }
-        return "Usuário ou senha inválidos";
+       if (usuario == null) {
+        return "Usuário não possui cadastro no sistema";
     }
+    
+    // Se o usuário existe, mas a senha não confere (Cenário do TC_005 atual)
+    if (!usuario.getSenha().equals(senha)) {
+        return "A senha se encontra errada";
+    }
+    
+    return "Bem vindo ao sistema";
+}       
+       //if (usuario != null) //-- adicionado para testar o tc002, tcc003 
+        
+       //if (usuario != null && usuario.getSenha().equals(senha)) { -- comentado para implementar o código para TC005
+
+          //  return "Bem vindo ao sistema"; -- comentado para implementar o código para TC005
+       // } -- comentado para implementar o código para TC005
+       // return "Usuário ou senha inválidos"; -- comentado para implementar o código para TC005
+   // } -- comentado para implementar o código para TC005
 }
