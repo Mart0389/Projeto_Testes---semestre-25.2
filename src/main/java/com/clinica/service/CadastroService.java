227aa7d19a65281ed31466c87c80ceeb98586132
Marta <martinha03luna@gmail.com>
2026-03-10 08:49:01 -0300

    TC007 -  parte green

--- src/main/java/com/clinica/service/CadastroService.java
+++ src/main/java/com/clinica/service/CadastroService.java
@@ -1,5 +1,18 @@
 package com.clinica.service;
 
+import com.clinica.model.Usuario;
+import com.clinica.repository.UsuarioRepository;
+
 public class CadastroService {
-    
+
+    private UsuarioRepository repository;
+
+    public CadastroService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public String cadastrar(Usuario usuario) {
+        
+        return "Cadastro realizado com sucesso";
+    }
 }
